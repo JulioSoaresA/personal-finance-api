280e4845386cd048202a0b810107bb78f8146bcb
Júlio Soares <juliocsoaresa1@gmail.com>
2025-11-21 12:50:05 -0300

    feat: add user registration and profile views with serializers 🧑‍🤝‍🧑

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets, filters
+from rest_framework.throttling import UserRateThrottle
+from django.contrib.auth.models import User
+from django_filters.rest_framework import DjangoFilterBackend
+from users.serializers import UserSerializer
 
-# Create your views here.
+class UserListView(mixins.ListAPIView, viewsets.GenericViewSet):
+    serializer_class = UserSerializer
+    throttle_classes = [UserRateThrottle]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['username', 'email']
+    
+    def get_queryset(self):
+        queryset = User.objects.all().exclude(pk=self.request.user.id)
+        
+        return queryset
+
+class UserProfileView(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    serializer_class = UserSerializer
+    throttle_classes = [UserRateThrottle]
+
+    def get_object(self):
+        return self.request.user
