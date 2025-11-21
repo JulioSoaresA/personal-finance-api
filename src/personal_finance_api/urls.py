b1b924a1b524207254832fb4f6f921691f885347
Júlio Soares <juliocsoaresa1@gmail.com>
2025-11-21 11:26:19 -0300

    feat: update dependencies and enhance settings for JWT and Swagger integration 🛠️

--- src/personal_finance_api/urls.py
+++ src/personal_finance_api/urls.py
@@ -1,22 +1,28 @@
-"""
-URL configuration for personal_finance_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Personal Finance API",
+        default_version='v1',
+        description="API documentation for the Personal Finance application",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
