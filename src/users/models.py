2eac0eb9b34a0d168d62847c6dd888817e81466d
Júlio Soares <juliocsoaresa1@gmail.com>
2025-11-29 17:21:46 -0300

    feat: implement BaseModel and update User model with UUID and timestamps 💾

--- src/users/models.py
+++ src/users/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from core.models import BaseModel
+from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
+class User(AbstractUser, BaseModel):
     CURRENCY_CHOICES = (
         ("USD", "US Dollar"),
         ("BRL", "Brazilian Real"),
         ("EUR", "Euro"),
     )
+    email = models.EmailField(_("email address"), unique=True)
 
     default_currency = models.CharField(
         max_length=3, choices=CURRENCY_CHOICES, default="USD"
     )
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
     def __str__(self) -> str:  # pragma: no cover - trivial
-        return self.username
+        return self.email
