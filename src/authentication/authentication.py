b784d74fcdbda1a734568f26378a1782eb5e0fc1
Júlio Soares <juliocsoaresa1@gmail.com>
2025-12-09 20:58:23 -0300

    feat: Add header-based JWT authentication and delete empty transactions test file.

--- src/authentication/authentication.py
+++ src/authentication/authentication.py
@@ -4,6 +4,16 @@ from rest_framework_simplejwt.exceptions import TokenError
 
 class CookiesJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
+        header = self.get_header(request)
+        if header is not None:
+            raw_token = self.get_raw_token(header)
+            if raw_token is not None:
+                try:
+                    validated_token = self.get_validated_token(raw_token)
+                    return self.get_user(validated_token), validated_token
+                except TokenError:
+                    pass
+
         access_token = request.COOKIES.get("access_token")
         if not access_token:
             return None
