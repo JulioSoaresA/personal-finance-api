4d0bd07221f887927f0f3f03cef8dd954c8ee1f3
Júlio Soares <juliosoares@b2bit.company>
2026-05-02 12:14:32 -0300

    refactor: remove unused ValidationError import from authentication errors module

--- src/authentication/errors.py
+++ src/authentication/errors.py
@@ -1,4 +1,4 @@
-from rest_framework.exceptions import APIException, ValidationError
+from rest_framework.exceptions import APIException
 from rest_framework import status
 from django.utils.translation import gettext_lazy as _
 
